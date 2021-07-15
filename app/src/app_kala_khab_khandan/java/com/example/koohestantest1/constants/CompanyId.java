4a4134bdfc908a3cbe4c88f99441f18d4dea62e9
Hooman <hooman.hooshyari@gmail.com>
2021-07-15 13:15:26 +0430

    Create new version

--- app/src/app_kala_khab_khandan/java/com/example/koohestantest1/constants/CompanyId.java
+++ app/src/app_kala_khab_khandan/java/com/example/koohestantest1/constants/CompanyId.java
@@ -4,6 +4,6 @@ import com.example.koohestantest1.R;
 
 //hello this is main app
 public class CompanyId {
-    public static final String COMPANY_ID = "CfHflGfFeey";
+    public static final String COMPANY_ID = "CWJJdxZXpEp";
     public static final int COMPANY_IMAGE = R.drawable.finallogo;
 }
